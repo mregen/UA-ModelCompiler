262fe29e97fa3fe18ce6daf409f02c479c88722d
Marc Schier <marcschier@hotmail.com>
2017-04-05 17:42:58 +0200

    Add async wcf stubs/interfaces

--- ModelCompiler/StackGenerator/DotNet/Templates/Interfaces/OperationAsyncEndpoint.cs
+++ ModelCompiler/StackGenerator/DotNet/Templates/Interfaces/OperationAsyncEndpoint.cs
@@ -2,6 +2,7 @@ class Placeholder
 {
 // ***START***
 #if (!OPCUA_EXCLUDE__NAME_)
+#if (!OPCUA_ASYNC_TASK)
 /// <summary>
 /// The operation contract for the _NAME_ service.
 /// </summary>
@@ -13,6 +14,15 @@ IAsyncResult Begin_NAME_(_NAME_Message request, AsyncCallback callback, object a
 /// The method used to retrieve the results of a _NAME_ service request.
 /// </summary>
 _NAME_ResponseMessage End_NAME_(IAsyncResult result);
+
+#else
+/// <summary>
+/// The operation contract for the _NAME_ service.
+/// </summary>
+[OperationContractAttribute(Action=Namespaces._ServicesNamespace_ + "/_NAME_", ReplyAction = Namespaces._ServicesNamespace_ + "/_NAME_Response")]
+[FaultContract(typeof(ServiceFault), Action = Namespaces._ServicesNamespace_ + "/_NAME_Fault", Name = "ServiceFault", Namespace = Namespaces._TypesNamespace_)]
+System.Threading.Tasks.Task<_NAME_ResponseMessage> _NAME_Async(_NAME_Message request);
+#endif
 #endif
 // ***END***
 }
